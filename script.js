da4d1600db6d3833a57f85683a4b12c922957eea
Parvathy R <parvathy07r@gmail.com>
2024-07-28 12:58:45 +0530

    feat: added function to populate UI with author's data

--- script.js
+++ script.js
@@ -3,14 +3,35 @@ const authorContainer = document.getElementById("author-container");
 //select html element with id #load-more-btn
 const loadMoreBtn = document.getElementById("load-more-btn");
 
+let startingIndex = 0;
+let endingIndex = 8;
+let authorDataArr = [];
+
 //to make a get request to the url
 fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
-.then((res) => {
-    console.log(res.json());
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((err) => {
-    console.log(`There was an error: ${err}`);
-});
+    .then((res) => {
+        console.log(res.json());
+    })
+    .then((data) => {
+        authorDataArr = data;
+        displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+    })
+    .catch((err) => {
+        console.log(`There was an error: ${err}`);
+    });
+
+//function to populate UI with author's data
+const displayAuthors = (authors) => {
+    authors.forEach(({ author, image, url, bio }, index) => {
+        authorContainer.innerHTML += `
+        <div id="${index}" class="user-card">
+        <h2 class="author-name">${author}</h2>
+        <img class="user-img" src="${image}" alt="${author} avatar">
+        <p class="bio">${bio}</p>
+        <a class="author-link" href="${url}" target="_blank">${author}'s author page </a>
+        </div>
+        `;
+    });
+}
+
+
